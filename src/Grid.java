dd3c3bfed6d9d02f9518322476744f8e3e5cba87
Remrem08007 <40875433+Remrem08007@users.noreply.github.com>
2023-02-19 11:39:40 -0500

    Update Grid.java

--- src/Grid.java
+++ src/Grid.java
@@ -1,30 +1,44 @@
-public class Grid {
-    public class Cell {
-        // TODO
+public class grid {
+    public class Grid {
 
-        public Cell(){
-            // TODO
-            return;
+        private Cell[] grid = new Cell[12];
+
+        public class Cell {
+
+            private Cell up = null;
+            private Cell down = null;
+            private Cell left = null;
+            private Cell right = null;
+
+            private int value;
+
+            public Cell(Cell up, Cell down, Cell left, Cell right, Integer value) {
+                
+                this.up = up;
+                this.down = down;
+                this.left = left;
+                this.right = right;
+
+                this.value = value;
+            }
         }
-    }
-    
-    // TODO
 
-    public Grid(){
-        // TODO
-    }
+        public Grid() {
+            // TODO
+        }
 
-    public boolean move(Cell box){
-        // TODO
-        return true;
-    }
+        public boolean move(Cell box) {
+            // TODO
+            return true;
+        }
 
-    public boolean check_complete(){
-        // TODO
-        return true;
-    }
+        public boolean check_complete() {
+            // TODO
+            return true;
+        }
 
-    public void solve_game(){
-        // TODO
+        public void solve_game() {
+            // TODO
+        }
     }
 }
